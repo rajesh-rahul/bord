bb1deefa528852aa4c018ecb71ea8be46ba6cfc8
rajesh-rahul <rahul-rajesh@outlook.com>
2025-01-23 19:08:36 -0700

    chore: Only expose two APIs for parsing

--- sqlite3-parser/src/lib.rs
+++ sqlite3-parser/src/lib.rs
@@ -1,18 +1,23 @@
-//! This is a parser to parse SQLite3(v.3.46.0 onwards). It follows the excellent error 
-//! resillient parsing [guide]((https://matklad.github.io/2023/05/21/resilient-ll-parsing-tutorial.html)) 
+//! This is a parser to parse SQLite3(v.3.46.0 onwards). It follows the excellent error
+//! resillient parsing [guide]((https://matklad.github.io/2023/05/21/resilient-ll-parsing-tutorial.html))
 //! by matklad
 
 mod cst;
+mod cursor;
+mod grammar;
+mod lexer;
 mod parser;
 mod token_kind;
 mod tree_kind;
+pub mod ungram;
 mod version;
-mod grammar;
-mod cursor;
-mod lexer;
 
-pub use cst::{ChildNodeKey, NodeId, SqliteNode, SqliteToken, SqliteUntypedAst};
 
+pub use cst::{
+    ChildNodeKey, NodeId, SqliteNode, SqliteToken, SqliteUntypedAst, TokenChild, TreeChild,
+};
+
+use enumset::EnumSet;
 pub use token_kind::{sqlite_keywords, SqliteTokenKind, MAX_KEYWORD_LEN};
 
 pub use tree_kind::SqliteTreeKind;
@@ -21,6 +26,32 @@ pub use version::SqliteVersion;
 
 pub use parser::{SqliteParseError, SqliteParser};
 
-pub use lexer::{SqliteLexer, LexError};
+pub use lexer::{LexError, SqliteLexer};
 
-pub use grammar::parse;
+pub mod ast;
+
+#[cfg(feature = "test_utils")]
+pub mod test_utils;
+
+pub fn parse(text: &str) -> SqliteUntypedAst {
+    let (tokens, _) = SqliteLexer::new(text, SqliteVersion([3, 46, 0])).lex();
+
+    let mut p = SqliteParser::new(tokens);
+    grammar::file(&mut p, Default::default());
+
+    let result = p.build_tree();
+
+    result
+}
+
+pub fn parse_any(
+    text: &str,
+    r: EnumSet<SqliteTokenKind>,
+    parse_function: fn(&mut SqliteParser, EnumSet<SqliteTokenKind>),
+) -> SqliteUntypedAst {
+    let (tokens, _) = SqliteLexer::new(text, SqliteVersion([3, 46, 0])).lex();
+    let mut p = SqliteParser::new(tokens);
+    parse_function(&mut p, r);
+
+    p.build_tree()
+}
