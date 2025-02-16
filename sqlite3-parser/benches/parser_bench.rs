f1fbb2b812ec3eded264a5c6fe53526550b4854e
rajesh-rahul <rahul-rajesh@outlook.com>
2025-02-16 07:46:34 -0700

    chore: Cleanup benchmarks

--- sqlite3-parser/benches/parser_bench.rs
+++ sqlite3-parser/benches/parser_bench.rs
@@ -1,3 +1,4 @@
+use bord_sqlite3_parser::{batch, incr};
 use criterion::{criterion_group, criterion_main, Criterion};
 use fallible_iterator::FallibleIterator;
 use sqlite3_parser::lexer::sql::Parser as LemonParser;
@@ -6,20 +7,27 @@ use sqlite3_parser::lexer::sql::Parser as LemonParser;
 /// 13% improvement when allocating the cst up front
 fn parser_benchmark(c: &mut Criterion) {
     // Create a benchmark group
-    let mut group = c.benchmark_group("Parser");
+    let mut group = c.benchmark_group("Parser Sakila Bench");
 
     let input_sql = include_str!("../../schema.sql");
 
-    group.bench_function("Bord Parser", |b| {
+    group.bench_with_input("Bord Incremental Parser", input_sql, |b, s| {
         b.iter(|| {
-            let cst = bord_sqlite3_parser::parse(input_sql);
-            assert!(cst.errors().next().is_none());
+            let _cst: incr::IncrSqlCst = bord_sqlite3_parser::parse(s);
+            // assert!(cst.errors().next().is_none());
         })
     });
 
-    group.bench_function("Lemon-rs Parser", |b| {
+    group.bench_with_input("Bord Batch/Regular Parser", input_sql, |b, s| {
         b.iter(|| {
-            let mut parser = LemonParser::new(input_sql.as_bytes());
+            let _cst: batch::SqlCst = bord_sqlite3_parser::parse(s);
+            // assert!(cst.errors().next().is_none());
+        })
+    });
+
+    group.bench_with_input("Lemon-rs Parser", input_sql.as_bytes(), |b, s| {
+        b.iter(|| {
+            let mut parser = LemonParser::new(s);
             loop {
                 let cmd = parser.next().unwrap();
 
@@ -40,19 +48,38 @@ fn basic_queries(c: &mut Criterion) {
     let mut group = c.benchmark_group("sqlite3_parser parsing benchmark");
 
     let string = "SELECT * FROM `table` WHERE 1 = 1;";
-    group.bench_with_input("sqlite3_parser::select", &string.as_bytes(), |b, &s| {
-        b.iter(|| {
-            let mut parser = LemonParser::new(s);
-            assert!(parser.next().unwrap().unwrap().readonly());
-        });
-    });
+    group.bench_with_input(
+        "Lemon-rs Parser - Select query",
+        &string.as_bytes(),
+        |b, &s| {
+            b.iter(|| {
+                let mut parser = LemonParser::new(s);
+                assert!(parser.next().unwrap().unwrap().readonly());
+            });
+        },
+    );
 
-    group.bench_with_input("bord_parser::select", &string, |b, &s| {
-        b.iter(|| {
-            let cst = bord_sqlite3_parser::parse(s);
-            assert!(cst.errors().next().is_none());
-        });
-    });
+    group.bench_with_input(
+        "Bord Incremental Parser - Select query",
+        &string,
+        |b, &s| {
+            b.iter(|| {
+                let _cst: incr::IncrSqlCst = bord_sqlite3_parser::parse(s);
+                // assert!(cst.errors().next().is_none());
+            });
+        },
+    );
+
+    group.bench_with_input(
+        "Bord Batch/Regular Parser - Select query",
+        &string,
+        |b, &s| {
+            b.iter(|| {
+                let _cst: batch::SqlCst = bord_sqlite3_parser::parse(s);
+                // assert!(cst.errors().next().is_none());
+            });
+        },
+    );
 
     let with_query = "
         WITH derived AS (
@@ -66,7 +93,7 @@ fn basic_queries(c: &mut Criterion) {
         LEFT JOIN derived USING (user_id);
     ";
     group.bench_with_input(
-        "sqlite3_parser::with_select",
+        "Lemon-rs Parser - WithSelect query",
         &with_query.as_bytes(),
         |b, &s| {
             b.iter(|| {
@@ -76,12 +103,27 @@ fn basic_queries(c: &mut Criterion) {
         },
     );
 
-    group.bench_with_input("bord_parser::with_select", &with_query, |b, &s| {
-        b.iter(|| {
-            let cst = bord_sqlite3_parser::parse(s);
-            assert!(cst.errors().next().is_none());
-        });
-    });
+    group.bench_with_input(
+        "Bord Incremental Parser - WithSelect query",
+        &with_query,
+        |b, &s| {
+            b.iter(|| {
+                let _cst: incr::IncrSqlCst = bord_sqlite3_parser::parse(s);
+                // assert!(cst.errors().next().is_none());
+            });
+        },
+    );
+
+    group.bench_with_input(
+        "Bord Batch/Regular Parser - WithSelect query",
+        &with_query,
+        |b, &s| {
+            b.iter(|| {
+                let _cst: batch::SqlCst = bord_sqlite3_parser::parse(s);
+                // assert!(cst.errors().next().is_none());
+            });
+        },
+    );
 }
 
 criterion_group!(benches, parser_benchmark, basic_queries);
