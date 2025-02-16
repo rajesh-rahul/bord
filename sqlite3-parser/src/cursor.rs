42e793c14b0246b5631391f05bc160a5c2140617
rajesh-rahul <rahul-rajesh@outlook.com>
2025-02-16 01:57:24 -0700

    feat: Remove abs_pos in Lexer

--- sqlite3-parser/src/cursor.rs
+++ sqlite3-parser/src/cursor.rs
@@ -1,11 +1,13 @@
 use std::{iter::Peekable, str::Chars};
 // TODO: Make nicer
+#[derive(Clone)]
 pub struct LexCursor<'a> {
     pub abs_position: u32,
     pub input: Chars<'a>,
     token_builder: TokenBuilder<'a>,
 }
 
+#[derive(Clone)]
 pub struct TokenBuilder<'a> {
     chars: Peekable<Chars<'a>>,
     curr_byte_len: usize,
@@ -38,7 +40,7 @@ impl<'a> TokenBuilder<'a> {
 }
 
 impl<'a> LexCursor<'a> {
-    pub fn new(input: &'a str) -> LexCursor {
+    pub fn new(input: &'a str) -> LexCursor<'a> {
         LexCursor {
             abs_position: 0,
             input: input.chars(),
@@ -76,18 +78,16 @@ impl<'a> LexCursor<'a> {
     }
 
     fn bump_original(&mut self) {
-        if let Some(_) = self.input.next() {
-            self.abs_position += 1;
+        if let Some(ch) = self.input.next() {
+            self.abs_position += ch.len_utf8() as u32; // what is this supposed to be?
         }
     }
 
-    pub fn build_token_info(&mut self) -> (&'a str, u32) {
+    pub fn build_token_text(&mut self) -> &str {
         let (token, _) = self
             .input
             .as_str()
             .split_at(self.token_builder.curr_byte_len);
-
-        let abs_pos = self.abs_position;
 
         for _ in 0..self.token_builder.curr_char_len {
             self.bump_original();
@@ -95,13 +95,10 @@ impl<'a> LexCursor<'a> {
 
         self.token_builder.reset();
 
-        (token, abs_pos)
+        token
     }
 
-    pub fn build_error_token_info(
-        &mut self,
-        is_err_predicate: impl Fn(char) -> bool,
-    ) -> (&'a str, u32) {
+    pub fn build_error_token_info(&mut self, is_err_predicate: impl Fn(char) -> bool) -> &str {
         // Atleast one character must be consumed or else we will be stuck in an infinite loop
         if self.token_builder.curr_char_len == 0 {
             self.next();
@@ -115,6 +112,6 @@ impl<'a> LexCursor<'a> {
             }
         }
 
-        self.build_token_info()
+        self.build_token_text()
     }
 }
