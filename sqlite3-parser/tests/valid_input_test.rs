e4e00589f65ddd121f631ad589a3ca2664597d95
rajesh-rahul <rahul-rajesh@outlook.com>
2025-02-25 03:57:17 -0700

    feat: Incorporate new CSTs into tests

--- sqlite3-parser/tests/valid_input_test.rs
+++ sqlite3-parser/tests/valid_input_test.rs
@@ -19,7 +19,26 @@ fn test_from_corpus() {
         input_string.split("%%").for_each(|test_data| {
             println!("data: {test_data}");
             let (sql, expected_ast_as_str) = test_data.split_once("\n\n").unwrap();
-            let ast = bord_sqlite3_parser::parse(sql);
+
+            let events_and_tokens = bord_sqlite3_parser::parse_events_and_tokens(sql);
+
+            // Test Incremental CST
+            let ast: incr::IncrSqlCst = bord_sqlite3_parser::parse(sql);
+            assert_eq!(events_and_tokens, ast.to_events_and_tokens());
+
+            crate::test_utils::check_input(&ast, expected_ast_as_str);
+            crate::test_utils::ensure_ast_conforms_to_ungram(&ast);
+
+            // Test Batch CST
+            let ast: batch::SqlCst = bord_sqlite3_parser::parse(sql);
+            assert_eq!(events_and_tokens, ast.to_events_and_tokens());
+
+            crate::test_utils::check_input(&ast, expected_ast_as_str);
+            crate::test_utils::ensure_ast_conforms_to_ungram(&ast);
+
+            // Test Batch CST
+            let ast: slot::SlotIncrSqlCst = bord_sqlite3_parser::parse(sql);
+            assert_eq!(events_and_tokens, ast.to_events_and_tokens());
 
             crate::test_utils::check_input(&ast, expected_ast_as_str);
             crate::test_utils::ensure_ast_conforms_to_ungram(&ast);
