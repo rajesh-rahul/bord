5ff16b648fbd5987b79d1615a230a3b28c931074
rajesh-rahul <rahul-rajesh@outlook.com>
2025-02-25 03:59:54 -0700

    feat: Add different incr parser entry points (WIP)

--- sqlite3-parser/src/lib.rs
+++ sqlite3-parser/src/lib.rs
@@ -3,30 +3,31 @@
 //! by matklad
 
 // mod cst;
-mod cst2;
+// mod cst2;
+mod cst;
 mod cursor;
 mod grammar;
 mod lexer;
-mod new_cst;
 mod parser;
 mod token_kind;
 mod tree_kind;
 pub mod ungram;
 mod version;
 
-// pub use new_cst::*;
+pub use cst::*;
 
-pub use cst2::{
-    ChildNodeKey, CstNode, CstNodeData, CstNodeDataKind, LexError, ModifiedBranchesInfo, NodeId,
-    SqliteToken, SqliteUntypedCst, TextPatch, TextPatchKind,
-};
+// pub use cst2::{
+//     ChildNodeKey, CstNode, CstNodeData, CstNodeDataKind, LexError, ModifiedBranchesInfo, NodeId,
+//     SqliteToken, SqliteUntypedCst, TextPatch, TextPatchKind,
+// };
 
 // use cst::incr::{IncrCstNode, IncrSqlCst};
 // pub use cst::*;
 
 use enumset::EnumSet;
 use grammar::utils::STATEMENT_START;
-use parser::new_on_demand_lexer;
+use parser::{new_on_demand_lexer, Event};
+use text_size::TextSize;
 pub use token_kind::{sqlite_keywords, SqliteTokenKind, MAX_KEYWORD_LEN};
 
 pub use tree_kind::SqliteTreeKind;
@@ -38,22 +39,38 @@ pub use parser::{
 };
 
 pub use lexer::SqliteLexer;
-
+pub use text_size;
 // pub mod ast;
 
 #[cfg(feature = "test_utils")]
 pub mod test_utils;
 
-pub fn parse(text: &str) -> SqliteUntypedCst {
+pub fn parse_with_abs_pos<CST: CstTrait>(abs_pos: TextSize, text: &str) -> CST {
     let lexer = SqliteLexer::new(text, SqliteVersion([3, 46, 0]));
 
-    let mut p = SqliteParser::new(NormalLexer::from(lexer));
+    let mut p = SqliteParser::with_abs_pos(NormalLexer::from(lexer), abs_pos);
     grammar::file(&mut p, Default::default());
 
     p.build_cst()
 }
 
-pub fn incremental_parse(text: &str, text_patch: TextPatch<usize, usize>) -> SqliteUntypedCst {
+pub fn parse<CST: CstTrait>(text: &str) -> CST {
+    parse_with_abs_pos(TextSize::new(0), text)
+}
+
+pub fn parse_events_and_tokens(text: &str) -> (Vec<Event>, Vec<SqliteToken>) {
+    let lexer = SqliteLexer::new(text, SqliteVersion([3, 46, 0]));
+
+    let mut p = SqliteParser::with_abs_pos(NormalLexer::from(lexer), TextSize::new(0));
+    grammar::file(&mut p, Default::default());
+
+    p.to_events_and_tokens()
+}
+
+pub fn incremental_parse(
+    text: &str,
+    text_patch: TextPatch<TextSize, TextSize>,
+) -> (Vec<Event>, Vec<SqliteToken>) {
     let on_demand_lexer = new_on_demand_lexer(text, SqliteVersion([3, 46, 0]));
     let mut p = SqliteParser::with_abs_pos(on_demand_lexer, text_patch.relex_start);
 
@@ -71,7 +88,12 @@ pub fn incremental_parse(text: &str, text_patch: TextPatch<usize, usize>) -> Sql
             // p.expected(SqliteTreeKind::Statement, STATEMENT_START);
         }
 
+        let was_at_semicolon = p.at(T![;]);
         p.must_eat(T![;], r);
+
+        if !was_at_semicolon {
+            continue;
+        }
 
         // NOTE: It seems that affected_node_by_len is an overestimate (prolly not worth fixing though)
         let expected_len = text_patch.start - text_patch.relex_start
@@ -79,17 +101,73 @@ pub fn incremental_parse(text: &str, text_patch: TextPatch<usize, usize>) -> Sql
             + text_patch.affected_node_byte_len;
         let actual_len = p.curr_byte_len();
 
-        let spillover = actual_len as i64 - expected_len as i64;
+        let spillover = actual_len.checked_add(expected_len);
 
         // If we consumed all tokens that were added during this text patch, go through the loop
         // the loop one more time to deal with edge cases like dangling semicolons
-        if spillover > 0 {
+        if spillover.is_some() {
             if extra_loops > 0 {
                 break;
             } else {
                 extra_loops += 1;
             }
         }
+
+        p.eat_trivia();
+    }
+
+    p.eat_trivia();
+
+    p.close(m, SqliteTreeKind::File);
+
+    p.to_events_and_tokens()
+}
+
+pub fn incremental_parse2<Cst: CstTrait>(
+    text: &str,
+    text_patch: TextPatch<TextSize, TextSize>,
+) -> Cst {
+    let on_demand_lexer = new_on_demand_lexer(text, SqliteVersion([3, 46, 0]));
+    let mut p = SqliteParser::with_abs_pos(on_demand_lexer, text_patch.relex_start);
+
+    let m = p.open();
+    let r = STATEMENT_START | T![;];
+
+    let extra_loops = 0;
+    while !p.eof() {
+        p.eat_trivia();
+
+        if p.at_any(STATEMENT_START) {
+            grammar::statement(&mut p, r);
+        } else {
+            expected_one_of!(p, r, SqliteTreeKind::Statement);
+            // p.expected(SqliteTreeKind::Statement, STATEMENT_START);
+        }
+
+        let was_at_semicolon = p.at(T![;]);
+        p.must_eat(T![;], r);
+
+        if !was_at_semicolon {
+            continue;
+        }
+
+        // NOTE: It seems that affected_node_by_len is an overestimate (prolly not worth fixing though)
+        // let expected_len = text_patch.start - text_patch.relex_start
+        //     + text_patch.size
+        //     + text_patch.affected_node_byte_len;
+        // let actual_len = p.curr_byte_len();
+
+        // let spillover = actual_len as i64 - expected_len as i64;
+
+        // // If we consumed all tokens that were added during this text patch, go through the loop
+        // // the loop one more time to deal with edge cases like dangling semicolons
+        // if spillover > 0 {
+        //     if extra_loops > 0 {
+        //         break;
+        //     } else {
+        //         extra_loops += 1;
+        //     }
+        // }
 
         p.eat_trivia();
     }
@@ -105,7 +183,7 @@ pub fn parse_any(
     text: &str,
     r: EnumSet<SqliteTokenKind>,
     parse_function: fn(&mut SqliteParser<NormalLexer>, EnumSet<SqliteTokenKind>),
-) -> SqliteUntypedCst {
+) -> incr::IncrSqlCst {
     let lexer = SqliteLexer::new(text, SqliteVersion([3, 46, 0]));
 
     let mut p = SqliteParser::new(NormalLexer::from(lexer));
@@ -126,7 +204,7 @@ fn simple_parser_test() {
         SELECT * FROM `table`
         LEFT JOIN derived USING (user_id);";
 
-    let cst = parse(input);
+    let cst: batch::SqlCst = parse(input);
 
     println!("{cst}");
 
@@ -142,19 +220,14 @@ fn simple_parser_test() {
 
 #[test]
 fn simple_parser_test2() {
-    let cst = parse("\n\n\n");
+    let cst: incr::IncrSqlCst = parse("\n\n\n");
 
     assert_eq!("\n\n\n", cst.root().to_text());
 }
 
 #[test]
 fn simple_parser_test3() {
-    let cst = parse(
-        "SELECT * FROM users;
+    let cst: incr::IncrSqlCst = parse("CREATE TABLE f     ");
 
-
-S",
-    );
-
-    println!("{cst}");
+    println!("{:#?}", cst.root().comparable());
 }
